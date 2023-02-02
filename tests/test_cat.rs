03caf9f424ee42f6242682466cb415ac19dd9805
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-02-01 16:24:40 -0800

    Refactored integration tests for reusability

--- tests/test_cat.rs
+++ tests/test_cat.rs
@@ -1,93 +1,72 @@
-//! Testing cat:
-//! There are some problems, namely with setup and teardown, especially
-//! without a testing framework. Here is a blog about how to do it:
-//! https://medium.com/@ericdreichert/test-setup-and-teardown-in-rust-without-a-framework-ba32d97aa5ab
-//!
-//! For now, a work around of using a struct that implements Drop was
-//! implemented. To prevent data race, run with:
-//! cargo test -- --test-threads 1
-use std::error::Error;
 use std::fs::{ self, File };
 use std::io::Write;
 use assert_cmd::Command;
+mod common;
 
-type TestResult = Result<(), Box<dyn Error>>;
+fn create_test_data() -> common::TestResult {
+    fs::create_dir_all("tests/inputs")?;
+    let mut foobar = File::create("tests/inputs/foobar")?;
+    writeln!(&mut foobar, "foobar")?;
 
-struct Setup;
+    let _empty = File::create("tests/inputs/empty")?;
 
-impl Setup {
-    /// Write some dummy files to a some temporary location. Call chmod on one of
-    /// them to make it inaccessible. Cargo test must be run at project root to
-    /// ensure the correct pathing
-    fn run() -> TestResult {
-        fs::create_dir_all("tests/inputs")?;
-        let mut foobar = File::create("tests/inputs/foobar")?;
-        writeln!(&mut foobar, "foobar")?;
+    let mut haiku = File::create("tests/inputs/haiku")?;
+    writeln!(&mut haiku, "It's not DNS")?;
+    writeln!(&mut haiku, "There's no way it's DNS")?;
+    writeln!(&mut haiku, "It was DNS")?;
 
-        let _empty = File::create("tests/inputs/empty")?;
-
-        let mut haiku = File::create("tests/inputs/haiku")?;
-        writeln!(&mut haiku, "It's not DNS")?;
-        writeln!(&mut haiku, "There's no way it's DNS")?;
-        writeln!(&mut haiku, "It was DNS")?;
-
-        let _not_allowed = File::create("tests/inputs/notallowed")?;
-        Command::new("chmod")
-            .args(["000", "tests/inputs/notallowed"])
-            .output()?;
-
-        return Ok(());
-    }
-}
-
-impl Drop for Setup {
-    fn drop(&mut self) {
-        let _ = fs::remove_dir_all("tests/inputs");
-    }
-}
-
-
-fn test(args: &[&str], stdin: &'static str, stdout_pred: &'static str, stderr_pred: &'static str, success: bool) -> TestResult {
-    let _ = Setup::run();
-    let mut assertion = Command::cargo_bin("cat")?
-        .args(args)
-        .write_stdin(stdin)
-        .assert();
-    
-    assertion = match success {
-        true => assertion.success(),
-        false => assertion.failure(),
-    };
-    assertion = assertion.try_stdout(stdout_pred)?;
-    _ = assertion.try_stderr(stderr_pred)?;
+    let _not_allowed = File::create("tests/inputs/notallowed")?;
+    Command::new("chmod")
+        .args(["000", "tests/inputs/notallowed"])
+        .output()?;
 
     return Ok(());
 }
 
+fn cleanup_test_data() {
+    let _ = fs::remove_dir_all("tests/inputs");
+}
+
 /// Conatenate several files and check the output
 #[test]
-fn cat_regular_files() -> TestResult {
-    return test(&["tests/inputs/foobar", "tests/inputs/foobar"], "", "foobar\nfoobar\n", "", true);
+fn cat_regular_files() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
+        &["tests/inputs/foobar", "tests/inputs/foobar"],
+        "",
+        true,
+        "foobar\nfoobar\n",
+        ""
+    );
 }
 
 /// Concatenate several files with stdin
 #[test]
-fn cat_files_and_stdin() -> TestResult {
-    return test(
+fn cat_files_and_stdin() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
         &["tests/inputs/foobar", "-", "tests/inputs/foobar"],
         "baz\n",
+        true,
         "foobar\nbaz\nfoobar\n",
         "",
-        true
     );
 }
 
 /// Concatenate with the -b flag, across oneor more files
 #[test]
-fn cat_counting_nonblank_lines() -> TestResult {
-    return test(
+fn cat_counting_nonblank_lines() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
         &["-b", "tests/inputs/haiku", "-", "tests/inputs/haiku"],
         "\n",
+        true,
         "     1	It's not DNS
      2	There's no way it's DNS
      3	It was DNS
@@ -97,16 +76,19 @@ fn cat_counting_nonblank_lines() -> TestResult {
      3	It was DNS
 ",
         "",
-        true
     );
 }
 
 /// Concatenate with the -n flag, across one or more files
 #[test]
-fn cat_counting_all_lines() -> TestResult {
-    return test(
+fn cat_counting_all_lines() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
         &["-n", "tests/inputs/haiku", "-", "tests/inputs/haiku"],
         "\n",
+        true,
         "     1	It's not DNS
      2	There's no way it's DNS
      3	It was DNS
@@ -116,27 +98,34 @@ fn cat_counting_all_lines() -> TestResult {
      3	It was DNS
 ",
         "",
-        true
+    );
+}
+
+#[test]
+fn cat_does_not_exist() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
+        &["does-not-exist"],
+        "",
+        false,
+        "",
+        "cat: does-not-exist: No such file or directory (os error 2)\n",
     );
 }
 
 /// Concatenate "does not exist" and "is not permitted"
 #[test]
-fn cat_inaccessible_files() -> TestResult {
-    test(
-        &["does-not-exist"],
-        "",
-        "",
-        "cat: does-not-exist: No such file or directory (os error 2)\n",
-        false
-    )?;
-    
-    test(
+fn cat_not_allowed() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "cat",
         &["tests/inputs/notallowed"],
         "",
+        false,
         "",
         "cat: tests/inputs/notallowed: Permission denied (os error 13)\n",
-        false
-    )?;
-    return Ok(());
+    );
 }
--- tests/test_head.rs
+++ tests/test_head.rs
@@ -1,123 +1,35 @@
 //! Test head:
-use std::error::Error;
-use std::fmt::{ Display, Formatter, Result as FormatResult };
 use std::fs::{ self, File };
 use std::io::Write;
 use assert_cmd::Command;
+mod common;
 
-type TestResult = Result<(), Box<dyn Error>>;
-
-/// Abstraction of the setup process before each test
-struct Setup;
-
-impl Setup {
-    /// Write some dummy files to a some temporary location. Call chmod on one of
-    /// them to make it inaccessible. Cargo test must be run at project root to
-    /// ensure the correct pathing
-    fn run() -> TestResult {
-        fs::create_dir_all("tests/inputs")?;
-        let mut manylines = File::create("tests/inputs/manylines")?;
-        for line_no in 0..100 {
-            writeln!(&mut manylines, "{:04}", line_no)?;
-        }
-
-        let _not_allowed = File::create("tests/inputs/notallowed")?;
-        Command::new("chmod")
-            .args(["000", "tests/inputs/notallowed"])
-            .output()?;
-
-        return Ok(());
+fn create_test_data() -> common::TestResult {
+    fs::create_dir_all("tests/inputs")?;
+    let mut manylines = File::create("tests/inputs/manylines")?;
+    for line_no in 0..100 {
+        writeln!(&mut manylines, "{:04}", line_no)?;
     }
-}
 
-impl Drop for Setup {
-    fn drop(&mut self) {
-        let _ = fs::remove_dir_all("tests/inputs");
-    }
-}
-
-/// The error to return when some test fails
-#[derive(Debug)]
-struct TestFailureError {
-    failure_msg: String,
-}
-
-impl TestFailureError {
-    fn new(msg: &str) -> Self {
-        return Self {
-            failure_msg: msg.to_string(),
-        };
-    }
-}
-
-impl Error for TestFailureError {}
-
-impl Display for TestFailureError {
-    fn fmt(&self, f: &mut Formatter) -> FormatResult {
-        write!(f, "{}", self.failure_msg)
-    }
-}
-
-/// Run a single test on the system implementation of "head". This function is
-/// useful for testing the tests
-#[allow(dead_code)]
-fn test_reference_bin(
-    args: &[&str],
-    stdin: &str,
-    success: bool,
-    stdout_pred: &str,
-    stderr_pred: &str,
-) -> TestResult {
-    _ = Setup::run();
-    let output = Command::new("head")
-        .args(args)
-        .write_stdin(stdin)
+    let _not_allowed = File::create("tests/inputs/notallowed")?;
+    Command::new("chmod")
+        .args(["000", "tests/inputs/notallowed"])
         .output()?;
-    if output.status.success() != success {
-        return Err(Box::new(TestFailureError::new("exit statuses don't match")));
-    }
-    let stdout = String::from_utf8(output.stdout)?;
-    let stderr = String::from_utf8(output.stderr)?;
-    if stdout != stdout_pred {
-        let error_msg = format!("stdout: expected '{stdout_pred}', got '{stdout}'");
-        return Err(Box::new(TestFailureError::new(&error_msg)));
-    }
-    if stderr != stderr_pred {
-        let error_msg = format!("stderr: expected '{stderr_pred}', got '{stderr}'");
-        return Err(Box::new(TestFailureError::new(&error_msg)));
-    }
 
     return Ok(());
 }
 
-fn test_cargo_bin(
-    args: &[&str],
-    stdin: &str,
-    success: bool,
-    stdout_pred: &'static str,
-    stderr_pred: &'static str,
-) -> TestResult {
-    _ = Setup::run();
-    let mut assertion = Command::cargo_bin("head")?
-        .args(args)
-        .write_stdin(stdin)
-        .assert();
-    if success {
-        assertion = assertion.try_success()?;
-    } else {
-        assertion = assertion.try_failure()?;
-    }
-
-    assertion = assertion.try_stdout(stdout_pred)?;
-    _ = assertion.try_stderr(stderr_pred)?;
-
-    return Ok(());
+fn cleanup_test_data() {
+    let _ = fs::remove_dir_all("tests/inputs");
 }
 
 /// Without any inputs, the head command should print the first ten lines
 #[test]
-fn single_file() -> TestResult {
-    return test_cargo_bin(
+fn single_file() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["-n", "4", "tests/inputs/manylines"],
         "",
         true,
@@ -127,8 +39,11 @@ fn single_file() -> TestResult {
 }
 
 #[test]
-fn byte_flag() -> TestResult {
-    return test_cargo_bin(
+fn byte_flag() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["-c", "10", "tests/inputs/manylines"],
         "",
         true,
@@ -140,8 +55,11 @@ fn byte_flag() -> TestResult {
 /// Without any file specified, the head command will listen for inputs
 /// from stdin.
 #[test]
-fn head_stdin() -> TestResult {
-    return test_cargo_bin(
+fn head_stdin() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["-n", "4"],
         "0\n1\n2\n3\n4\n5\n6\n7\n8\n9",
         true,
@@ -153,8 +71,11 @@ fn head_stdin() -> TestResult {
 /// When heading multiple files, the content of each file is prefixed with a
 /// header and separated by a line break.
 #[test]
-fn multiple_files() -> TestResult {
-    return test_cargo_bin(
+fn multiple_files() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["-n", "1", "tests/inputs/manylines", "tests/inputs/manylines"],
         "",
         true,
@@ -171,8 +92,11 @@ fn multiple_files() -> TestResult {
 /// If the input file does not exist, output to stderr:
 /// "head: does-not-exist: No such file or directory"
 #[test]
-fn does_not_exist() -> TestResult {
-    return test_cargo_bin(
+fn does_not_exist() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["does-not-exist"],
         "",
         false,
@@ -184,8 +108,11 @@ fn does_not_exist() -> TestResult {
 /// If the input file cannot be read, output to stderr:
 /// "head: not-allowed: Permission denied"
 #[test]
-fn permission_denied() -> TestResult {
-    return test_cargo_bin(
+fn permission_denied() -> common::TestResult {
+    return common::test_cargo_bin(
+        Box::new(create_test_data),
+        Box::new(cleanup_test_data),
+        "head",
         &["tests/inputs/notallowed"],
         "",
         false,
