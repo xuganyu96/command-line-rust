93654b2de0b2a91b83ec60e7a732fdabed9d9575
Ganyu "Bruce" Xu <xuganyu@berkeley.edu>
2023-02-07 21:15:46 -0800

    A somewhat working prototype

--- tests/test_uniq.rs
+++ tests/test_uniq.rs
@@ -1,56 +1,75 @@
-//! Integration tests for my implementation of the "uniq" program
-use std::error::Error;
+//! Integration tests for uniq, including the following variations:
+//! input can come from stdin or some file
+//! output can go to stdout or some file
+//! use the "count" flag or not
+//! several different inputs
+use std::{
+    fs::File,
+    io::{ BufReader, BufRead },
+    error::Error
+};
+use tempfile::NamedTempFile;
 use assert_cmd::Command;
-
-const PRG: &str = "uniq";
 
 type TestResult = Result<(), Box<dyn Error>>;
 
-/// The type of input for uniq, which can be either stdin or a file
-enum InputType {
-    /// If the input comes from stdin, then the content is the input itself
-    STDIN(&'static str),
-
-    /// If the input comes from a file, then the content is the file path
-    FilePath(&'static str),
-}
-
-/// The type of output for uniq, which can be either stdou or a file
-enum OutputType {
-    STDOUT,
-    FilePath(&'static str),
-}
-
-/// Encapsulation of a single integration test for a binary (system binary or
-/// cargo binary)
-struct IntegrationTest {
-    bin: Command,
-    input: InputType,
-    output: OutputType,
-    expect_success: bool,
-    stdout_pred: &'static str,
-    stderr_pred: &'static str,
-}
-
-impl IntegrationTest {
-    /// Instantiate a new instance of the struct
-    fn new() -> Self { todo!(); }
-
-    /// Execute the test as specified at instantiation
-    fn run(self) -> TestResult {
-        todo!();
+/// Execute the input Command (system binary or cargo binary) once using the
+/// specified inputs and compare with the specified outputs. If filepath is
+/// supplied, then the content will be checked against fileout.
+///
+/// The caller is responsible for supplying the path and the content of the
+/// output file in both "args" and "output_path"
+fn test_bin(
+    mut bin: Command,
+    args: &[&str],
+    stdin: &str,
+    success: bool,
+    stdout: &'static str,
+    stderr: &'static str,
+    output_path: Option<&str>,
+    output_str: Option<&'static str>,
+) -> TestResult {
+    let mut assertion = bin.args(args)
+        .write_stdin(stdin)
+        .assert();
+    if success {
+        assertion = assertion.try_success()?;
+    } else {
+        assertion = assertion.try_failure()?;
     }
+
+    assertion = assertion.try_stdout(stdout)?;
+    let _ = assertion.try_stderr(stderr)?;
+
+    // Check file output
+    if let Some(output_path) = output_path {
+        let output_str = output_str.unwrap(); // assume they are both Some()
+        let mut reader = BufReader::new(File::open(output_path)?);
+        let mut output = String::new();
+        while let Ok(bytes) = reader.read_line(&mut output) {
+            if bytes == 0 { break; }
+        }
+        assert_eq!(output, output_str);
+    }
+
+    return Ok(());
 }
 
 #[test]
-fn something() {
-    let mut cmd = Command::new(PRG);
-    let mut cmd = Command::cargo_bin(PRG).unwrap();
+fn cities() -> TestResult {
+    let outfile = NamedTempFile::new()?;
+    let cmd = Command::new("uniq");
 
-    cmd.args(["-"])
-        .write_stdin("Hello, world!\n")
-        .assert()
-        .success()
-        .stdout("Hello, world!\n")
-        .stderr("");
+    test_bin(
+        cmd,
+        &["-", outfile.path().to_str().unwrap()],
+        "Madrid\nMadrid\nLisbon\n",
+        true,
+        "",
+        "",
+        Some(outfile.path().to_str().unwrap()),
+        Some("Madrid\nLisbon\n"),
+    )?;
+
+    return Ok(());
 }
